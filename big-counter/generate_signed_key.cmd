4db31afca03dd4bbbd22971f151817981fbb062d
unknown <holobeat@gmail.com>
2021-08-23 23:26:05 -0400

    web ui update

---
+++ big-counter/generate_signed_key.cmd
@@ -0,0 +1,1 @@
+"C:\Program Files\Android\Android Studio\jre\bin\keytool.exe" -genkey -v -keystore c:\Users\gelsner\upload-keystore.jks -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias upload
